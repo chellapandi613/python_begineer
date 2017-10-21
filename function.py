fe205904f208a7a3140255d1d49bc0ca74f9c015
chellapandi613 <32951409+chellapandi613@users.noreply.github.com>
2017-10-21 15:34:49 +0530

    Add files via upload

---
+++ function.py
@@ -0,0 +1,24 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      16cse006
+#
+# Created:     21/10/2017
+# Copyright:   (c) 16cse006 2017
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+
+ if __name__ == '__main__':
+    main()
+def my_function():
+    print "Hello From My Function!"
+
+def my_function_with_args(first,sec):
+    print first + sec
+
+
+my_function()
+my_function_with_args("Your name ", "have a great year!")
