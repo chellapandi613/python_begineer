a7077d19afee3ccda80dcaa6bc046941ed3d962e
chellapandi613 <32951409+chellapandi613@users.noreply.github.com>
2017-10-21 14:05:46 +0530

    Add files via upload

---
+++ condition statement.py
@@ -0,0 +1,23 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      16cse006
+#
+# Created:     21/10/2017
+# Copyright:   (c) 16cse006 2017
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+   a=8
+   if(a<10):
+    print("a is less then 10")
+   elif(a<10 and a>15):
+        print("a is in between")
+   else:
+    print("a is stupid")
+
+
+if __name__ == '__main__':
+    main()
